411a3cb51fed6c1ea54515e29b476a2beb7a8e51
Rameshwar94 <rameshwar.chavan.0321@gmail.com>
2022-08-06 11:25:47 +0530

    first commit

---
+++ src/test/java/com/javatutorials/discoverymicroserviceserver/DiscoveryMicroserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatutorials.discoverymicroserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryMicroserviceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
